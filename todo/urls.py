4b32e05838517ab63b2660cdcbb9a15c155fbd80
Arjun Komath <arjunkomath@gmail.com>
2016-05-02 22:45:17 +0530

    restful

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,7 @@ from todoapp import views, api
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^api/todo', api.todo),
+	url(r'^api/todo/$', api.todo),
+	url(r'^api/todo/(?P<id>\w+)$', api.todo),
 	url(r'^admin/', admin.site.urls),
 ]
--- todoapp/api.py
+++ todoapp/api.py
@@ -1,40 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed,QueryDict
 from django.core import serializers
 from django.db.models import Q
 import json, datetime
 
 from todoapp.models import *
 
-def todo(request):
-	query = request.GET.get('query', '');
-	if query != '': 
-		query = query.split(',')
-		pub_date = datetime.datetime.now()
+def todo(request, id = None):
 
-		todo = Todo()
-		todo.todo_text = query[0]
-		todo.created_at = pub_date
-		todo.save()
+	# read and read by id, @return json
+	if request.method == 'GET':
 
-		del query[0]
-
-		for tag in query:
-			if tag.strip() == '':
-				continue
+		if id != None:
 			try:
-				t = Tag.objects.get(slug=tag)
-				todo.tags.add(t)
-			except Tag.DoesNotExist:
-				t=Tag()
-				t.slug = tag
-				t.save()
-				todo.tags.add(t)
+				t = Todo.objects.get(pk=id)
+				res = serializers.serialize("json", [t])
+				return HttpResponse(res, content_type="application/json")
+			except Todo.DoesNotExist:
+				response = json.dumps({'errors': ['No Items found'], 'success': False})
+				return HttpResponse(response, content_type="application/json")
 
-		response = json.dumps({'query': query, 'success': True})
-		return HttpResponse(response, content_type="application/json")
-	else:
-		keyword = request.GET.get('keyword', '');
+		keyword = request.GET.get('keyword', '')
 
 		if keyword != '':
 			data = Todo.objects.order_by('-created_at').filter(Q(tags__slug__contains=keyword) | Q(todo_text__contains=keyword))
@@ -54,3 +40,92 @@ def todo(request):
 		list['tags'] = tags_data
 		response = json.dumps(list)
 		return HttpResponse(response, content_type="application/json")
+
+	# create, @return json
+	elif request.method == 'POST':
+
+		query = request.POST.get('query', '');
+
+		if query != '':
+			query = query.split(',')
+			pub_date = datetime.datetime.now()
+
+			todo = Todo()
+			todo.todo_text = query[0]
+			todo.created_at = pub_date
+			todo.save()
+
+			del query[0]
+
+			for tag in query:
+				if tag.strip() == '':
+					continue
+				try:
+					t = Tag.objects.get(slug=tag)
+					todo.tags.add(t)
+				except Tag.DoesNotExist:
+					t=Tag()
+					t.slug = tag
+					t.save()
+					todo.tags.add(t)
+
+			response = json.dumps({'query': query, 'success': True})
+			return HttpResponse(response, content_type="application/json")
+		else:
+			return HttpResponseBadRequest('Query missing')
+
+	# delete, @return json
+	elif request.method == 'DELETE':
+		if id == None:
+			return HttpResponseBadRequest('ID missing')
+		else:
+			try:
+				t = Todo.objects.get(pk=id)
+				t.delete()
+				response = json.dumps({'message': 'Item has been deleted', 'success': True})
+				return HttpResponse(response, content_type="application/json")
+			except Todo.DoesNotExist:
+				response = json.dumps({'errors': ['No Items found'], 'success': False})
+				return HttpResponse(response, content_type="application/json")
+
+	# delete, @return json
+	elif request.method == 'PUT':
+		if id == None:
+			return HttpResponseBadRequest('ID missing')
+		else:
+			request.PUT = QueryDict(request.body)
+			query = request.PUT.get('query', '');
+			if query != '':
+				query = query.split(',')
+
+				try:
+					todo = Todo.objects.get(pk=id)
+					todo.todo_text = query[0]
+					todo.tags.clear()
+					todo.save()
+
+					del query[0]
+
+					for tag in query:
+						if tag.strip() == '':
+							continue
+						try:
+							t = Tag.objects.get(slug=tag)
+							todo.tags.add(t)
+						except Tag.DoesNotExist:
+							t=Tag()
+							t.slug = tag
+							t.save()
+							todo.tags.add(t)
+
+					response = json.dumps({'message': 'Item has been updated', 'success': True})
+					return HttpResponse(response, content_type="application/json")
+				except Todo.DoesNotExist:
+					response = json.dumps({'errors': ['No Items found'], 'success': False})
+					return HttpResponse(response, content_type="application/json")
+
+			else:
+				return HttpResponseBadRequest('Query missing')
+
+	else:
+		return HttpResponseNotAllowed('Method not allowed')
